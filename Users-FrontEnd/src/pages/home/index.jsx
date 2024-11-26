688f53e9838c27902c0df075e12bac5f66ec56ce
matcb <mathcarvalhobarreto@gmail.com>
2024-11-26 06:40:28 -0300

    createUser() and getUser()

--- Users-FrontEnd/src/pages/home/index.jsx
+++ Users-FrontEnd/src/pages/home/index.jsx
@@ -1,17 +1,59 @@
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './style.css'
 import Garbage from '../../assets/garbage.svg'
+import api from '../../services/api'
 
 function Home() {
+  
+  const [users, setUsers] = useState([])
+
+  const inputData = [
+    {
+      "inputName": useRef()
+    },
+    {
+      "inputEmail": useRef()
+    },
+    {
+      "inputAge": useRef()
+    }
+    
+
+  ]
+
+  async function getUsers(){
+   const usersFromAPI = await api.get('/users')
+   setUsers(usersFromAPI.data)
+  }
+
+  async function createUser(){
+    api.post('/users', {
+      name: inputData[0].inputName.current.value,
+      email: inputData[1].inputEmail.current.value,
+      age: inputData[2].inputAge.current.value
+    })
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+
   return(
     <div className='container'>
 
       <form action="">
         <h1>Register Users</h1>
-        <input placeholder='name' type="text" name='name'/>
-        <input placeholder='email' type="email" name='email' />
-        <input placeholder='age' type="number" name="age"/>
-        <button type='submit'>Register</button>
+        <input placeholder='name' type="text" name='name' ref={inputData[0].inputName}/>
+        <input placeholder='email' type="email" name='email' ref={inputData[1].inputEmail} />
+        <input placeholder='age' type="number" name="age" ref={inputData[2].inputAge}/>
+        <button type='submit' onClick={createUser}>Register</button>
       </form>
 
     
